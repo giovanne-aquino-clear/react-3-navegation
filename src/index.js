5cb11de0c392dc5b6ed7c2625ec6b21ee4c1b6f2
giovanne <giovanne.aquino.clear@gmail.com>
2023-09-14 09:59:05 -0300

    App config and css for App

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import  ReactDOM  from 'react-dom'
+import React from 'react'
+import App from './views/App'
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+    <App/>,
+    document.getElementById('root')
+)
